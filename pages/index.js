7f76e11047e298d0be8967d0df9dea65a464bf6f
Isaac Tait <isaac@mountaintopcoding.com>
2021-11-28 21:12:07 -0500

    update tab title

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>I Love To Fish</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
