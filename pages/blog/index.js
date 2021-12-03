d2c012f127bb040f3e5a4390f5cf583d11bf42a8
Isaac Tait <isaac@mountaintopcoding.com>
2021-12-03 10:19:30 -0500

    add new page

---
+++ pages/blog/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Blog = () => {
+    return (
+        <div>
+        <h1 className="text-2xl">This is my blog</h1>
+        <Link></Link>
+        </div>
+    );
+}
+
+export default Blog;
---
+++ pages/blog/today.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Today = () => {
+    return (
+        <div>
+        <h1 className="text-2xl">Today</h1>
+        </div>
+    );
+}
+
+export default Today;
