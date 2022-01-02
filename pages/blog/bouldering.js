09dccd1bc60a957c4d674e3fd96b8819097edd9f
Isaac Tait <isaac@mountaintopcoding.com>
2022-01-02 14:45:07 -0500

    update blog

---
+++ pages/blog/bouldering.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Bouldering = () => {
+    return (
+        <div>
+            <h1 className='text-2xl font-semibold text-green-400'>Morgan R. Chaney Sanctuary</h1>
+        </div>
+    )
+}
+
+export default Bouldering;
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -4,8 +4,13 @@ import Link from 'next/link';
 const Blog = () => {
     return (
         <div>
-        <h1 className="text-2xl">This is my blog</h1>
-        <Link></Link>
+            <h1 className="text-2xl">Venue for my thoughts, adventures, and ideas. Read at your own risk...</h1>
+            <Link href="/blog/bouldering">
+                <a className='underline text-green-400'>
+                <p>Boulders</p>
+                </a>
+            </Link>
+            <p>I like to climb rocks. Recently I discoverd a collection of boulders about five minutes from my house...</p>
         </div>
     );
 }
