5003154e3718f0d8cc7a9f49d430dcae922b0cc2
Isaac Tait <isaac@mountaintopcoding.com>
2022-02-07 12:40:33 -0500

    add contact form

--- pages/contact.js
+++ pages/contact.js
@@ -1,11 +1,18 @@
 import Link from 'next/link'
 
+import { useForm, ValidationError } from "@formspree/react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import Header from '../pages/components/header'
 import Footer from '../pages/components/footer'
 
-const Contact = () => {
+export default function Contact() {
+    const [state, handleSubmit] = useForm("https://formspree.io/f/mdobeblw");
+
+    if (state.succeeded) {
+      return <p>Thank you for your submission!</p>;
+    }  
+
     return (
         <div className='heropattern-bubbles-neutral-100 h-screen'>
             <Header />
@@ -43,11 +50,29 @@ const Contact = () => {
                 </div>
             </header>
             <h2 className='font-semibold text-2xl text-center text-red-500'>How to get a hold of me...</h2>
+            
+            <form onSubmit={handleSubmit} className='max-w-6xl mx-auto bg-indigo-100'> 
+                <div className='flex flex-wrap justify-center pt-2'>
+                    <label htmlFor="email" className='mr-2'>Your Email Address</label>
+                    <input id="email" type="email" name="email" />
+                    <ValidationError prefix="Email" field="email" errors={state.errors} />
+                </div>
+                <div className='flex flex-wrap justify-center pt-6'>
+                    <label htmlFor="message" className='mr-2'>Your Message</label>
+                    <textarea id="message" name="message" className='box-content w-full h-60 mx-2'/>
+                    <ValidationError prefix="Message" field="message" errors={state.errors} />
+                </div>
+                <div className='flex flex-wrap justify-center py-8'>
+                    <button type="submit" disabled={state.submitting} className='bg-amber-400 rounded px-2 hover:text-white hover:underline'>
+                        Submit
+                    </button>
+                    <ValidationError errors={state.errors} />
+                </div>
+            </form>
+            
             <div className='fixed bottom-0 w-full'>
                 <Footer />
             </div>
         </div>
     )
 };
-
-export default Contact;
