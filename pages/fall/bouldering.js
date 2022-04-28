713b96db88670b1dcc698cfc2edbe7cc8d5fb6c9
Isaac Tait <isaac@mountaintopcoding.com>
2022-04-28 10:47:47 -0400

    chore: add breadcrumbs take 1 🍞

--- pages/fall/bouldering.js
+++ pages/fall/bouldering.js
@@ -2,15 +2,15 @@ import React from 'react';
 import Link from 'next/link'
 import Image from 'next/image'
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
-import Header from '../components/header'
-import Footer from '../components/footer'
+import Header from '../components/Header'
+import Breadcrumbs from '../components/NextBreadcrumbs'
+import Footer from '../components/Footer'
 
 const Bouldering = () => {
     return (
         <div className='heropattern-topography-neutral-100'>
             <Header />
+            <Breadcrumbs />
             <div className='h-screen max-w-6xl mx-auto overflow-y-scroll px-1'>
                 <h1 className='text-2xl font-semibold text-emerald-600 flex justify-center mb-8'>A climbers sanctuary, close to home</h1>
                 <p 
--- pages/fall/index.js
+++ pages/fall/index.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import Header from '../components/header';
-import Footer from '../components/footer';
+import Footer from '../components/Footer';
 
 const Blog = () => {
     return (
