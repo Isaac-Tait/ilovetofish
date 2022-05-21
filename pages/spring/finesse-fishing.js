77dcaca8fd47b577d82360de715c7d8e497397ef
Isaac Tait <isaac@mountaintopcoding.com>
2022-05-21 18:12:19 -0400

    chore: i am so confused 😬

--- pages/spring/finesse-fishing.js
+++ pages/spring/finesse-fishing.js
@@ -3,9 +3,9 @@ import Image from 'next/image'
 
 import Breadcrumbs from 'nextjs-breadcrumbs';
 
-import Comment from '../components/Comment'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import Comment from '../components/comment'
+import Header from '../components/header'
+import Footer from '../components/footer'
 
 const Finesse = () => {
     return (
