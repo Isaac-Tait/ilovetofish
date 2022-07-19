ed19e31662e67e4b55d4639afe4f38fee7c2c258
Isaac Tait <isaac@mountaintopcoding.com>
2022-07-19 16:36:09 -0400

    feat: replace disqus with giscus 🏎

--- components/Comment.js
+++ components/Comment.js
@@ -1,18 +1,23 @@
-import {DiscussionEmbed} from "disqus-react"
+import React from "react";
 
-const Comment = ({ slug, title }) => {    
-    return (
-      <div>
-        <DiscussionEmbed
-            shortname="iloveto-fish"
-            config={{
-                url: `https://iloveto.fish/${slug}`,
-                identifier: '123',
-                title,
-            }}
-        />
-      </div>
-    )
-  } 
-  
-  export default Comment;
+import Giscus from '@giscus/react';
+
+export default function Comments() {
+  return (
+    <Giscus
+      id="comments"
+      repo="Isaac-Tait/comments-ilovetofish"
+      repoId="R_kgDOHr1TDw"
+      category="General"
+      categoryId="DIC_kwDOHr1TD84CQUWk"
+      mapping="pathname"
+      term="Welcome to @giscus/react component!"
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="bottom"
+      theme="light"
+      lang="en"
+      loading="lazy"
+    />
+  );
+}
