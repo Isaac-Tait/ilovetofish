5669c31f8b0e6ed457591b5f0d0e20253eb87f33
Isaac Tait <isaac@mountaintopcoding.com>
2022-08-29 21:15:21 -0400

    chore: add instagram logo and link to about 🛸

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,8 @@
 import Link from 'next/link'
-
 import Image from 'next/image'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
@@ -60,6 +61,11 @@ const About = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="text-blue-500 h-12 w-12 my-8" viewBox="0 0 16 16">
                         <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
                     </svg>
+                </a>
+                <a href='https://www.instagram.com/iloveto.fish/' target="_blank" rel="noopener noreferrer">
+                    <div className="pt-8 ml-4">
+                    <Image src='/instagram.png' alt="instagram logo" width={50} height={50} />
+                    </div>
                 </a>              
             </div>
                 
