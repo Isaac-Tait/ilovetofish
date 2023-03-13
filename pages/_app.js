f366212e788b958e062175d790c7a592938e0b42
Isaac Tait <isaac@mountaintopcoding.com>
2023-03-13 10:59:49 -0400

    chore: font awesome can be frustrating 🤬

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,12 @@
-import 'tailwindcss/tailwind.css'
+import 'tailwindcss/tailwind.css';
 import 'nextjs-breadcrumbs/dist/index.css';
 
-const { library, config } = require('@fortawesome/fontawesome-svg-core');
-import { fal } from '@fortawesome/pro-light-svg-icons'
-config.autoAddCss = false
-
-library.add(fal)
+const { config } = require('@fortawesome/fontawesome-svg-core');
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
-import Head from 'next/head'
-import Link from 'next/link'
+import Head from 'next/head';
+import Link from 'next/link';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function Home() {
   return (
     <div className='heropattern-bubbles-neutral-100 h-screen'>
       <Head>
         <title>I Love To Fish</title>
-        <link rel="icon" href="/favicon.png" />
+        <link rel='icon' href='/favicon.png' />
       </Head>
 
       <div className='flex flex-col'>
@@ -20,46 +20,57 @@ export default function Home() {
         </div>
         <main className='pt-16'>
           <div className='grid grid-cols-1 gap-0 md:grid-cols-6 md:gap-4 max-w-6xl mx-auto'>
-            
             <h3 className='grid-cols-1 md:col-start-2 md:col-span-4 h-20 md:h-48 flex justify-center mb-8 md:mb-0'>
-              <Link href="/spring" legacyBehavior>
-                <a className="text-lime-400 hover:text-blue-600 my-auto">
-                  <FontAwesomeIcon icon={['fal', 'flower-daffodil']} className='h-14 hover:animate-spin-slow' />
+              <Link href='/spring' legacyBehavior>
+                <a className='text-lime-400 hover:text-blue-600 my-auto'>
+                  <FontAwesomeIcon
+                    icon={['fal', 'flower-daffodil']}
+                    className='h-14 hover:animate-spin-slow'
+                  />
                 </a>
               </Link>
             </h3>
 
             <h3 className='grid-cols-1 md:col-start-1 md:col-end-3 h-20 md:h-48 flex justify-center mb-8 md:mb-0'>
-              <Link href="/summer" legacyBehavior>
-                <a className="text-rose-400 hover:text-indigo-500 my-auto">
-                  <FontAwesomeIcon icon={['fal', 'cloud-sun-rain']} className='h-14 hover:animate-spin-slow' />
+              <Link href='/summer' legacyBehavior>
+                <a className='text-rose-400 hover:text-indigo-500 my-auto'>
+                  <FontAwesomeIcon
+                    icon={['fal', 'cloud-sun-rain']}
+                    className='h-14 hover:animate-spin-slow'
+                  />
                 </a>
               </Link>
             </h3>
 
             <h3 className='grid-cols-1 md:col-end-8 md:col-span-3 h-20 md:h-48 flex justify-center mb-8 md:mb-0'>
-              <Link href="/fall" legacyBehavior>
-                <a className="text-fuchsia-400 hover:text-indigo-500 my-auto">
-                  <FontAwesomeIcon icon={['fal', 'leaf-maple']} className='h-14 hover:animate-spin-slow' />
+              <Link href='/fall' legacyBehavior>
+                <a className='text-fuchsia-400 hover:text-indigo-500 my-auto'>
+                  <FontAwesomeIcon
+                    icon='fa-brands fa-canadian-maple-leaf'
+                    className='h-14 hover:animate-spin-slow'
+                  />
                 </a>
-                </Link>
+              </Link>
             </h3>
 
             <h3 className='grid-cols-1 md:col-start-1 md:col-span-7 h-20 md:h-48 flex justify-center mb-8 md:mb-0'>
-              <Link href="/winter" legacyBehavior>
-                <a className="text-teal-400 hover:text-indigo-500 my-auto">
-                  <FontAwesomeIcon icon={['fal', 'snowflakes']} className='h-14 hover:animate-spin-slow'/>
+              <Link href='/winter' legacyBehavior>
+                <a className='text-teal-400 hover:text-indigo-500 my-auto'>
+                  <FontAwesomeIcon
+                    icon='fa-regular fa-snowflake'
+                    className='h-14 hover:animate-spin-slow'
+                  />
                 </a>
               </Link>
             </h3>
           </div>
         </main>
-        <p></p>    
+        <p></p>
       </div>
-      
+
       <div className='fixed bottom-0 w-full'>
         <Footer />
       </div>
     </div>
-  )
+  );
 }
