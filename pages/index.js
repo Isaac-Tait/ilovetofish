837a48217cb0c7dd0298abd886e2341ffd1d75d4
Isaac Tait <isaac@mountaintopcoding.com>
2023-10-21 06:54:21 -0400

    feat: update favicon 🐟

--- pages/index.js
+++ pages/index.js
@@ -10,47 +10,47 @@ import Footer from '../components/Footer'
 
 export default function Home() {
   return (
-    <div className="heropattern-bubbles-neutral-100 h-screen">
+    <div className="h-screen heropattern-bubbles-neutral-100">
       <Head>
         <title>I Love To Fish</title>
-        <link rel="icon" href="/favicon.png" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <div className="pl-1">
         <Header />
       </div>
-      <main className="h-4/5 grid place-content-center">
+      <main className="grid h-4/5 place-content-center">
         <div className="flex flex-col md:flex-row">
-          <h3 className="mx-14 mb-8 md:mb-0 hover:animate-spin-slow">
+          <h3 className="mx-14 mb-8 hover:animate-spin-slow md:mb-0">
             <Link href="/spring" legacyBehavior>
-              <a className="text-lime-400 hover:text-blue-500 flex flex-col">
+              <a className="flex flex-col text-lime-400 hover:text-blue-500">
                 <FontAwesomeIcon icon={faRainbow} className="text-5xl" />
                 <p className="text-center">Spring</p>
               </a>
             </Link>
           </h3>
 
-          <h3 className="mx-14 mb-8 md:mb-0 hover:animate-spin-slow">
+          <h3 className="mx-14 mb-8 hover:animate-spin-slow md:mb-0">
             <Link href="/summer" legacyBehavior>
-              <a className="text-rose-400 hover:text-indigo-500 flex flex-col">
+              <a className="flex flex-col text-rose-400 hover:text-indigo-500">
                 <FontAwesomeIcon icon={faSun} className="text-5xl" />
                 <p className="text-center">Summer</p>
               </a>
             </Link>
           </h3>
 
-          <h3 className="mx-14 mb-8 md:mb-0 hover:animate-spin-slow">
+          <h3 className="mx-14 mb-8 hover:animate-spin-slow md:mb-0">
             <Link href="/fall" legacyBehavior>
-              <a className="text-fuchsia-400 hover:text-indigo-500 flex flex-col">
+              <a className="flex flex-col text-fuchsia-400 hover:text-indigo-500">
                 <FontAwesomeIcon icon={faTree} className="text-5xl" />
                 <p className="text-center">Fall</p>
               </a>
             </Link>
           </h3>
 
-          <h3 className="mx-14 mb-8 md:mb-0 hover:animate-spin-slow">
+          <h3 className="mx-14 mb-8 hover:animate-spin-slow md:mb-0">
             <Link href="/winter" legacyBehavior>
-              <a className="text-teal-400 hover:text-indigo-500 flex flex-col">
+              <a className="flex flex-col text-teal-400 hover:text-indigo-500">
                 <FontAwesomeIcon icon={faSnowflake} className="text-5xl" />
                 <p className="text-center">Winter</p>
               </a>
