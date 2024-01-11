1d1db9d3b4c765a4d620ffaeb2a29c51a3f4492b
Isaac Tait <isaac@mountaintopcoding.com>
2024-01-11 07:50:32 -0500

    feat: add deconstruction blog post 🥋

---
+++ pages/winter/deconstruction.js
@@ -0,0 +1,165 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import Breadcrumbs from 'nextjs-breadcrumbs'
+
+import Comment from '../../components/Comment'
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
+
+const Blank = () => {
+  return (
+    <div className="heropattern-topography-neutral-100">
+      <Header />
+      <Breadcrumbs useDefaultStyle omitRootLabel labelsToUppercase={true} />
+      <div className="mx-auto h-screen max-w-6xl overflow-y-scroll px-1">
+        <h1 className="mb-8 flex justify-center text-2xl font-semibold text-emerald-600">
+          Namaste&nbsp;
+          <span role="img" aria-label="folded hands">
+            &#128591;
+          </span>
+        </h1>
+        <p
+          className="first-letter:float-left first-letter:mr-3
+                    first-letter:text-7xl first-letter:font-bold first-letter:text-slate-900
+                    first-line:uppercase first-line:tracking-widest"
+        >
+          Deconstructing and disentangling from an ultra conservative, far right, fundamentalist,
+          christian upbringing, indoctrination, and brainwashing can be a very intimidating process.{' '}
+          <Link
+            href="https://docs.eandp.cc/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-teal-400 underline hover:text-indigo-400"
+          >
+            I have been chronicling that journey
+          </Link>{' '}
+          through writing and social media, which to be honest has been an EXHAUSTING process.
+          Discovering that everything my faith had taught was 100% truth an undeniable fact was
+          actually just conjecture, bad theology based on shitty translations, ideas, and fairy
+          tales is a hard pill to swallow. Freeing myself from thirty plus years immersed in a cess
+          pool of fear, self-loathing, anger, resentment, patriarchy, and control has been a hard
+          difficult process but one that I would do again without a seconds hesitation.
+        </p>
+        <p className="mt-2">
+          So, that is what 2023 looked like for me. All of my available energy was absorbed by
+          undergoing this metamorphosis. Now if I had to pick a strain of doctrine for myself it
+          would be Buddhism. I tried on atheism for a few months but it just did not fit. Perhaps I
+          still have some fear to sort through instilled in me from the decades of phobia
+          indoctrination. What I do know though is that if there is a G-d and{' '}
+          <Link
+            href="https://en.wikipedia.org/wiki/That_All_Shall_Be_Saved"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-teal-400 underline hover:text-indigo-400"
+          >
+            universalism
+          </Link>{' '}
+          is not real then I want nothing to do with that G-d. The injustice, insanity, sadness,
+          horror, and down right vileness of anything less is just too much to believe in.
+        </p>
+        <div className="mt-2 flex justify-center">
+          <Image
+            src="https://res.cloudinary.com/mountaintopcoding-127956/image/upload/v1704975871/deconstruction%20-%20ilovetofish/bird_sanctuary-hike_pnuc6w.jpg"
+            height={750}
+            width={562}
+            className="rounded-lg"
+            alt={'Project'}
+          />
+        </div>
+        <p className="flex justify-center text-xs italic">
+          Hiking with my son in the wildlife sanctuary that abuts our property in SE Connecticut
+        </p>
+        <p className="mt-2">
+          Besides fishing I kicked my trail projects into overdrive resurrecting an old trail behind
+          our home and creating a network of trails on my small property to include some fun
+          mountain bike terrain features. I have also written 20,000+ words for{' '}
+          <Link
+            href="https://wormwoodsaga.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-teal-400 underline hover:text-indigo-400"
+          >
+            book two
+          </Link>{' '}
+          of my recently self published novel.{' '}
+          <Link
+            href="/summer/vacation"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-teal-400 underline hover:text-indigo-400"
+          >
+            We had a great summer
+          </Link>{' '}
+          filled with adventure and the fall was no less magical with a family RV trip to southern
+          Vermont and visiting family for Halloween (can I just say Halloween is so much more fun
+          when you are not a fundamentalist).
+        </p>
+        <div className="mt-2 flex justify-center">
+          <Image
+            src="https://res.cloudinary.com/mountaintopcoding-127956/image/upload/v1704975871/deconstruction%20-%20ilovetofish/sunset-home_rjfnmj.jpg"
+            height={750}
+            width={562}
+            className="rounded-lg"
+            alt={'Project'}
+          />
+        </div>
+        <p className="flex justify-center text-xs italic">
+          A lovely shot of my home on a sunset stroll
+        </p>
+        <p className="mt-2">
+          I celebrated my 40th birthday this past summer with friends and family in California. It
+          was a short trip but I did manage to squeeze in a climb to{' '}
+          <Link
+            href="https://res.cloudinary.com/mountaintopcoding-127956/image/upload/v1704976787/deconstruction%20-%20ilovetofish/san_jacinto_ght5rf.jpg"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-teal-400 underline hover:text-indigo-400"
+          >
+            the summit of San Jacinto
+          </Link>{' '}
+          via the aerial tram that I have wanted to do for a LONG time.
+        </p>
+        <div className="mt-2 flex justify-center">
+          <Image
+            src="https://res.cloudinary.com/mountaintopcoding-127956/image/upload/v1704975870/deconstruction%20-%20ilovetofish/great_glen_trails_vtmiw0.jpg"
+            height={750}
+            width={562}
+            className="rounded-lg"
+            alt={'Project'}
+          />
+        </div>
+        <p className="flex justify-center text-xs italic">
+          First ski tour of the season - Great Glen Trails in Gorham, NH
+        </p>
+        <p className="mt-2">
+          This season I kicked off a foray into a new hobby - telemark skiing. I got a pair of XCD
+          skis for the holidays - Fischer Traverse 78s with Rottefella xPlore bindings and{' '}
+          <Link
+            href="https://www.alfaoutdoor.com/products/free-a-p-s-gtx-m-black"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-teal-400 underline hover:text-indigo-400"
+          >
+            Alfa Free ski boots.
+          </Link>{' '}
+          My technique is still pretty rough but I have executed several successful telemark turns -
+          the most recent was earlier this week when I went for a tour of Winding Trails in
+          Farmington CT. So, yea 2023 was a tough year but it has also been one of growth, learning,
+          exploring, adventure, and wonder. Stay safe out there and... Namaste&nbsp;
+          <span role="img" aria-label="folded hands">
+            &#128591;
+          </span>
+        </p>
+      </div>
+
+      <div className="mx-auto mt-4 max-w-6xl">
+        <Comment />
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default Blank
--- pages/winter/index.js
+++ pages/winter/index.js
@@ -1,103 +1,104 @@
-import React from 'react';
-import Link from 'next/link';
+import React from 'react'
+import Link from 'next/link'
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSun } from '@fortawesome/free-regular-svg-icons';
-import { faRainbow, faTree } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSun } from '@fortawesome/free-regular-svg-icons'
+import { faRainbow, faTree } from '@fortawesome/free-solid-svg-icons'
 
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
 
 const Skiing = () => {
   return (
-    <div className='heropattern-topography-neutral-100'>
+    <div className="heropattern-topography-neutral-100">
       <Header />
-      <header className='lg:flex lg:flex-wrap lg:justify-center mx-1'>
-        <div className='lg:w-2/3 flex my-auto justify-between'>
-          <h3 className='text-2xl font-semibold mr-4'>
+      <header className="mx-1 lg:flex lg:flex-wrap lg:justify-center">
+        <div className="my-auto flex justify-between lg:w-2/3">
+          <h3 className="mr-4 text-2xl font-semibold">
             <Link href={'/spring'} legacyBehavior>
-              <a className='text-lime-400 hover:text-indigo-500'>
-                <FontAwesomeIcon icon={faRainbow} className='h-10' />
+              <a className="text-lime-400 hover:text-indigo-500">
+                <FontAwesomeIcon icon={faRainbow} className="h-10" />
               </a>
             </Link>
           </h3>
-          <h3 className='text-2xl font-semibold'>
+          <h3 className="text-2xl font-semibold">
             <Link href={'/summer'} legacyBehavior>
-              <a className='text-rose-400 hover:text-indigo-500'>
-                <FontAwesomeIcon icon={faSun} className='h-10' />
+              <a className="text-rose-400 hover:text-indigo-500">
+                <FontAwesomeIcon icon={faSun} className="h-10" />
               </a>
             </Link>
           </h3>
-          <h3 className='text-2xl font-semibold'>
+          <h3 className="text-2xl font-semibold">
             <Link href={'/fall'} legacyBehavior>
-              <a className='text-fuchsia-400 hover:text-indigo-500'>
-                <FontAwesomeIcon icon={faTree} className='h-10' />
+              <a className="text-fuchsia-400 hover:text-indigo-500">
+                <FontAwesomeIcon icon={faTree} className="h-10" />
               </a>
             </Link>
           </h3>
         </div>
       </header>
-      <div className='max-w-6xl mx-auto h-screen px-1'>
-        <h1 className='text-2xl bg-teal-400 flex flex-row justify-center text-neutral-100 rounded-xl mt-1'>
+      <div className="mx-auto h-screen max-w-6xl px-1">
+        <h1 className="mt-1 flex flex-row justify-center rounded-xl bg-teal-400 text-2xl text-neutral-100">
           Winter
         </h1>
-        <Link href='/winter/koselig' legacyBehavior>
-          <a className='underline text-amber-500'>
+        <Link href="/winter/koselig" legacyBehavior>
+          <a className="text-amber-500 underline">
             <p>Koselig</p>
           </a>
         </Link>
         <p>
-          Let us get cozy for a second and have a quick chat about a
-          positive mental state surrounding winter and all her cold,
-          dark, and gray glory...
+          Let us get cozy for a second and have a quick chat about a positive mental state
+          surrounding winter and all her cold, dark, and gray glory...
         </p>
 
-        <Link href='/winter/nordic-skiing' legacyBehavior>
-          <a className='underline text-amber-500'>
+        <Link href="/winter/nordic-skiing" legacyBehavior>
+          <a className="text-amber-500 underline">
             <p>Nordic Skiing</p>
           </a>
         </Link>
-        <p>
-          I thought you had to be wealthy to go skiing until I
-          discovered nordic skiing...
-        </p>
+        <p>I thought you had to be wealthy to go skiing until I discovered nordic skiing...</p>
 
-        <Link href='/winter/new-england' legacyBehavior>
-          <a className='underline text-amber-500'>
+        <Link href="/winter/new-england" legacyBehavior>
+          <a className="text-amber-500 underline">
             <p>New England</p>
           </a>
         </Link>
-        <p>
-          I have fallen in love with New England. I do not think I
-          will ever leave...
-        </p>
-        <Link href='/winter/father-son' legacyBehavior>
-          <a className='underline text-amber-500'>
+        <p>I have fallen in love with New England. I do not think I will ever leave...</p>
+        <Link href="/winter/father-son" legacyBehavior>
+          <a className="text-amber-500 underline">
             <p>Father Son Ski Trip</p>
           </a>
         </Link>
         <p>
-          I used to be entirely opposed to skiing during the holidays.
-          I tried it once and the crowds (and prices) were insane.
-          Well now that my son is in school I had no choice. Well it
-          was a great trip and I was pleasantly surprised by the
-          solitude and adventure...
+          I used to be entirely opposed to skiing during the holidays. I tried it once and the
+          crowds (and prices) were insane. Well now that my son is in school I had no choice. Well
+          it was a great trip and I was pleasantly surprised by the solitude and adventure...
         </p>
-        <Link href='/winter/100-mile-wilderness' legacyBehavior>
-          <a className='underline text-amber-500'>
+        <Link href="/winter/100-mile-wilderness" legacyBehavior>
+          <a className="text-amber-500 underline">
             <p>100 Mile Wilderness</p>
           </a>
         </Link>
         <p>
-          In the far north of Maine lies a wilderness area called the
-          100 Mile Wilderness. There are several huts that you can ski
-          to and from that make visiting this winter wonderland a
+          In the far north of Maine lies a wilderness area called the 100 Mile Wilderness. There are
+          several huts that you can ski to and from that make visiting this winter wonderland a
           truly special experience...
+        </p>
+
+        <Link href="/winter/deconstruction" legacyBehavior>
+          <a className="text-amber-500 underline">
+            <p>Deconstruction</p>
+          </a>
+        </Link>
+        <p>
+          2023 was an exhausting, challenging, and sometimes down right scary journey of growing,
+          stretching, unlearning, and learning. I fished a lot, not as much as last year but still a
+          decent amount...
         </p>
       </div>
       <Footer />
     </div>
-  );
-};
+  )
+}
 
-export default Skiing;
+export default Skiing
