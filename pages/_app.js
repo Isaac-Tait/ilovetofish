9a27be046af669dfd000199d5be8d4912fe8339e
Isaac Tait <isaac@mountaintopcoding.com>
2024-12-28 09:41:01 -0800

    fix: final vercel build error 🌮

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import 'tailwindcss/tailwind.css'
-import 'nextjs-breadcrumbs/dist/index.css'
 import { Kablammo } from 'next/font/google'
 
 import { SpeedInsights } from '@vercel/speed-insights/next'
