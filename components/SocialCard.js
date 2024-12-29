d481ac76e25d692996ac031c680c2a36dd76df6f
Isaac Tait <isaac@mountaintopcoding.com>
2024-12-29 10:26:21 -0800

    feat: iphone mockup is complete! 📱

--- components/SocialCard.js
+++ components/SocialCard.js
@@ -4,10 +4,10 @@ import Image from 'next/image'
 export default function SocialCard({ url, image, slug, title }) {
   return (
     <div className="">
-      <Link href={url} legacyBehavior>
-        <Image src={image} alt={title} width={100} height={100} className="h-32 w-32" />
+      <Link href={url} target="_blank" className="cursor-pointer">
+        <Image src={image} alt={title} width={100} height={100} className="h-32 w-32 p-1" />
       </Link>
-      <p className="w-full text-wrap bg-neutral-200">{slug}</p>
+      <p className="mx-auto mb-1 w-fit bg-neutral-200 p-1 text-xs italic">{slug}</p>
     </div>
   )
 }
--- pages/link.js
+++ pages/link.js
@@ -4,30 +4,36 @@ import Footer from '../components/Footer'
 import socialList from '../util/social'
 import SocialCard from '../components/SocialCard'
 
+import Image from 'next/image'
+
 const Social = () => {
   return (
     <div className="flex h-screen flex-col bg-gray-400">
       <Header />
-
       <div className="grow place-content-center overflow-y-scroll">
         {/* iPhone Mockup */}
-        <div class="relative mx-auto h-[600px] w-[320px] rounded-[2.5rem] border-[14px] border-gray-800 bg-gray-800 md:h-[500px] dark:border-gray-800">
+        <div className="relative mx-auto h-[600px] w-[320px] rounded-[2.5rem] border-[14px] border-gray-800 bg-gray-800 md:h-[550px] dark:border-gray-800">
           {/* Mute Button */}
-          <div class="absolute -start-[17px] top-[72px] h-[32px] w-[3px] rounded-s-lg bg-gray-800 dark:bg-gray-800"></div>
+          <div className="absolute -start-[17px] top-[72px] h-[32px] w-[3px] rounded-s-lg bg-gray-800 dark:bg-gray-800"></div>
           {/* Top Volume Button */}
-          <div class="absolute -start-[17px] top-[124px] h-[46px] w-[3px] rounded-s-lg bg-gray-800 dark:bg-gray-800"></div>
-          {/* Bottom Volum Button */}
-          <div class="absolute -start-[17px] top-[178px] h-[46px] w-[3px] rounded-s-lg bg-gray-800 dark:bg-gray-800"></div>
+          <div className="absolute -start-[17px] top-[124px] h-[46px] w-[3px] rounded-s-lg bg-gray-800 dark:bg-gray-800"></div>
+          {/* Bottom Volume Button */}
+          <div className="absolute -start-[17px] top-[178px] h-[46px] w-[3px] rounded-s-lg bg-gray-800 dark:bg-gray-800"></div>
           {/* Power Button */}
-          <div class="absolute -end-[17px] top-[142px] h-[64px] w-[3px] rounded-e-lg bg-gray-800 dark:bg-gray-800"></div>
+          <div className="absolute -end-[17px] top-[142px] h-[64px] w-[3px] rounded-e-lg bg-gray-800 dark:bg-gray-800"></div>
           {/* Screen Content */}
-          <div class="flex h-[572px] w-[292px] flex-wrap overflow-scroll rounded-[2rem] bg-neutral-400 md:h-[472px] dark:bg-gray-600">
-            <p className="p-2 text-center font-medium text-rose-500">
-              My <span className="italic">Super</span> Great Instagram Page
-            </p>
-            {socialList.map((d) => (
-              <SocialCard key={d.id} url={d.url} slug={d.slug} title={d.title} image={d.image} />
-            ))}
+          <div className="h-[572px] w-[292px] overflow-scroll rounded-[2rem] bg-neutral-100 p-4 md:h-[522px] dark:bg-gray-600">
+            {/* Screen Header */}
+            <div className="flex items-center justify-between p-2">
+              <p className="font-bold">iloveto.fish</p>
+              <Image src="/threadsLogo.png" height={20} width={20} alt={'threads'} />
+            </div>
+            {/* Image Cards */}
+            <div className="grid grid-cols-2 gap-2">
+              {socialList.map((d) => (
+                <SocialCard key={d.id} url={d.url} slug={d.slug} title={d.title} image={d.image} />
+              ))}
+            </div>
           </div>
         </div>
       </div>
