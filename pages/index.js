599556d112559153977dd5e772e9d43a7c823849
Isaac Tait <isaac@mountaintopcoding.com>
2024-12-29 11:43:26 -0800

    chore: make video responsive 🙊

--- pages/index.js
+++ pages/index.js
@@ -13,25 +13,22 @@ export default function Home() {
       </Head>
       <Header />
       <div className="grow place-content-center justify-between">
-        <main className="mt-6 flex flex-col justify-center">
+        <main className="mt-6 flex flex-col justify-between">
           <div
             style={{ fontFamily: 'var(--font-kablammo)' }}
             className="mx-auto w-fit rounded-2xl px-1 text-6xl font-bold tracking-wider text-lime-400"
           >
             <h1 className="mb-2 text-center">The Mountains Are Calling</h1>
           </div>
-          <div className="mx-auto">
-            <iframe
-              width="610"
-              height="365"
-              src="https://www.youtube.com/embed/1Ax3euD-pyE?si=Jwe7tVkvLs9T0LuZ"
-              title="YouTube video player"
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerpolicy="strict-origin-when-cross-origin"
-              allowfullscreen
-            ></iframe>
-          </div>
+          <iframe
+            className="mx-auto aspect-video w-full md:w-1/2"
+            src="https://www.youtube.com/embed/1Ax3euD-pyE?si=Jwe7tVkvLs9T0LuZ"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></iframe>
         </main>
       </div>
       <div className="sticky bottom-0">
